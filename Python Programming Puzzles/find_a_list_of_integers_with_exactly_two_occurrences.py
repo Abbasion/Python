7f423c198743df95623c152a24fc00d7fd41f9fc
Maaz Shoaib Abbasi <82872796+Abbasion@users.noreply.github.com>
2023-03-04 03:21:14 +0500

    Puzzles

---
+++ Python Programming Puzzles/find_a_list_of_integers_with_exactly_two_occurrences.py
@@ -0,0 +1,13 @@
+def test(nums):
+    return nums.count(19) == 2 and nums.count(5) >= 3
+
+nums = [19,19,15,5,3,5,5,2]
+print(test(nums))
+nums = [19,15,15,5,3,3,5,2]
+print(test(nums))
+nums = [19,19,5,5,5,5,5]
+print(test(nums))
+nums = [19,19,19,19,5,5,5,5,5]
+print(test(nums))
+nums = [19,19,5,5,19,19,5,5,5,5,5]
+print(test(nums))
