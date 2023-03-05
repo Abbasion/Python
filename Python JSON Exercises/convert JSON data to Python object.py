718fcce439f73094be991646921357174aabcd08
Maaz Shoaib Abbasi <82872796+Abbasion@users.noreply.github.com>
2023-03-05 23:37:02 +0500

    json exercise

---
+++ Python JSON Exercises/convert JSON data to Python object.py
@@ -0,0 +1,8 @@
+import json
+json_obj = '{ "Name":"Maaz", "Class":"12","Age":6}'
+python_obj = json.loads(json_obj)
+print("\nJSON data:")
+print(python_obj)
+print("\nName: ",python_obj["Name"])
+print("Class: ",python_obj["Class"])
+print("Age: ",python_obj["Age"]) 
