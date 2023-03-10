62e3b67ed3941b6f29130196961ac203a0895f79
Maaz Shoaib Abbasi <82872796+Abbasion@users.noreply.github.com>
2023-03-10 23:15:38 +0500

    commit

---
+++ little project/average_of_the_marks_array_for_the_student_name_provided.py
@@ -0,0 +1,16 @@
+if __name__ == '__main__':
+    n = int(input())
+    student_marks = {}
+    for _ in range(n):
+        name, *line = input().split()
+        scores = list(map(float, line))
+        student_marks[name] = scores
+    query_name = input()
+    print('%0.2f' % (sum(student_marks[query_name])/len(student_marks[query_name])))
+
+
+# 3
+# Krishna 67 68 69
+# Arjun 70 98 63
+# Malika 52 56 60
+# Malika
